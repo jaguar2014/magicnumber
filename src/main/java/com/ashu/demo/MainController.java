32f7b283163636856a4ac889e50de8a2c6eca42d
Ashenafi Mulat <emailmulat@gmail.com>
2018-01-26 10:48:16 -0500

    magic number initial commit

---
+++ src/main/java/com/ashu/demo/MainController.java
@@ -0,0 +1,39 @@
+package com.ashu.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/magicnumber")
+    public String showMagicNumber(HttpServletRequest param){
+
+        String number = param.getParameter("number");
+        if(number==null) {
+            return "Please enter a number greater than 0";
+        }
+        int startingNumber= Integer.parseInt(number);
+        String results = "Starting Number:  "+startingNumber+"<br />";
+        while (startingNumber>0) {
+            if (startingNumber % 2 == 0) {
+
+                startingNumber = startingNumber / 2;
+                results+=startingNumber+"<br />";
+                if(startingNumber==1) break;
+            } else if (startingNumber % 2 == 1) {
+                startingNumber = (startingNumber * 3) + 1;
+                results+=startingNumber+"<br />";
+                if(startingNumber==1) break;
+            }
+
+        }
+
+  return results;
+    }
+
+
+}
